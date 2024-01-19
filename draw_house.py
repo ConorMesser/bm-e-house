75d70831de730bcbf3568d06d75b67e021239b3e
Conor Messer <conormesser@C02XP00CJHD5.local>
2024-01-19 15:50:10 -0800

    Add primary draw_house.py file

---
+++ draw_house.py
@@ -0,0 +1,32 @@
+import turtle as t
+
+
+def draw_house():
+    """Draw house with garage, 1 door, 4 windows, 2 trees, and 2 clouds on blank canvas.
+
+    Note: Utilizes the turtle graphics package.
+    """
+    t.screensize(1500, 1500)
+
+    # draw house with height ~600, width ~500
+
+    # draw garage with height ~400, width ~300
+
+    # draw (2) garage doors on garage
+
+    # draw door on house
+
+    # draw three 2nd story windows (same size)
+
+    # draw one 1st story window (next to door)
+
+    # draw two trees (one on each side of house, slightly different heights)
+
+    # draw two clouds above house
+
+    t.mainloop()
+
+
+
+if __name__ == '__main__':
+    draw_house()
