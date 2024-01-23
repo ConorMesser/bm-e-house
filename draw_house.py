30add0b3beecc2f4e0107045ee34b37322c6536e
Nikki Mishra <nom@stanford.edu>
2024-01-22 18:29:03 -0800

    NM updated main function

--- draw_house.py
+++ draw_house.py
@@ -12,8 +12,13 @@ def draw_house():
     turtle_cursor = t.Turtle()
 
     # draw house with height ~600, width ~500
-
+    x_rect1 = 0
+    y_rect1 = 0
+    drawRectangle(turtle_cursor, x_rect1, y_rect1, 600, 500)
     # draw garage with height ~400, width ~300
+    x_rect2 = 500
+    y_rect2 = 0
+    drawRectangle(turtle_cursor, x_rect2, y_rect2, 400, 300)
 
     # draw (2) garage doors on garage
     draw_garage_door(turtle_cursor, 50, -50, 100, 100)
@@ -30,6 +35,13 @@ def draw_house():
     draw_branches(turtle_cursor, 75, 600, 0)
     
     # draw two clouds above house
+    x_cloud = 800
+    y_cloud = 800
+    myFlatCloud(turtle_cursor, x_cloud, y_cloud)
+    radius = 50 
+    x_bcloud = -800
+    y_bcloud = 800
+    myBumpyCloud(radius, x_bcloud, y_bcloud, cloud_color="blue")
 
     t.mainloop()
 
@@ -101,6 +113,62 @@ def draw_garage_door(t, start_x, start_y, width, height):
         t.goto(start_x, start_y + i * height / 10)
         t.pendown()
         t.forward(width)
+def drawRectangle(my_turtle, x_rect, y_rect, h, w):
+    my_turtle.pu()
+    my_turtle.setpos(x_rect,y_rect)
+    my_turtle.pd()
+    my_turtle.forward(w)
+    my_turtle.left(90)
+    my_turtle.forward(h)
+    my_turtle.left(90)
+    my_turtle.forward(w)
+    my_turtle.left(90)
+    my_turtle.forward(h)
+    my_turtle.pu()
+
+def myFlatCloud(my_turtle, x_cloud, y_cloud):
+    my_turtle.pu()
+    my_turtle.setpos(x_cloud,y_cloud)
+    my_turtle.pd()
+    my_turtle.forward(150)
+    my_turtle.left(90)
+    turt_pos = my_turtle.pos()
+    my_turtle.forward(30)
+
+    for i in range (1,3):
+        for i in range(1,15):
+            my_turtle.left(10)
+            my_turtle.forward(10)
+        my_turtle.left(210)
+
+        for i in range(1,20):
+            my_turtle.left(10)
+            my_turtle.forward(10) 
+        my_turtle.left(190)
+    my_turtle.left(160)
+    current_pos = my_turtle.pos()
+    #print((current_pos[1]-turt_pos[1]))
+    my_turtle.forward((current_pos[1]-turt_pos[1]))
+    my_turtle.left(90)
+    my_turtle.forward((turt_pos[0]-current_pos[0]))
+
+def ellipse(radius, color):
+    t.color(color,color)
+    t.begin_fill()
+    for i in range(2):
+        t.circle(radius,90)
+        t.circle(radius//2,90)
+    t.end_fill()
+
+def myBumpyCloud(radius, x_cloud, y_cloud, cloud_color="blue"):
+    my_turtle.pu()
+    my_turtle.setpos(x_cloud,y_cloud)
+    my_turtle.pd()
+    ellipse(radius,cloud_color)
+    t.forward(radius)
+    for i in range(1,7):
+        ellipse(radius,cloud_color)
+        t.right(60)
 
 if __name__ == '__main__':
     draw_house()
